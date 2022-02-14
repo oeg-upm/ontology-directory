163bb93bc677a794bf3a8d17dd587163f18c6eee
usuario <usuario@DESKTOP-OERVJCK>
2022-02-14 19:40:08 +0100

    Modificados los dos get para poder hacer sparql con GraphDB.

--- src/main/java/Service.java
+++ src/main/java/Service.java
@@ -1,71 +1,134 @@
 import static spark.Spark.*;
 import java.util.ArrayList;
+
+import org.eclipse.rdf4j.model.Value;
+import org.eclipse.rdf4j.query.Binding;
+import org.eclipse.rdf4j.query.BindingSet;
+import org.eclipse.rdf4j.query.QueryLanguage;
+import org.eclipse.rdf4j.query.TupleQuery;
+import org.eclipse.rdf4j.query.TupleQueryResult;
+import org.eclipse.rdf4j.repository.RepositoryConnection;
+import org.eclipse.rdf4j.repository.RepositoryException;
+import org.eclipse.rdf4j.repository.config.RepositoryConfigException;
+import org.eclipse.rdf4j.repository.http.HTTPRepository;
+import org.eclipse.rdf4j.rio.RDFHandlerException;
+import org.eclipse.rdf4j.rio.RDFParseException;
+
+import com.ontotext.graphdb.example.util.EmbeddedGraphDB;
+
+import java.io.IOException;
 import java.net.*;
 
 public class Service {
+	
+	//Método auxiliar al que pasándole la query a realizar hace todo lo requerido 
+	public static String llamadaDBGraph(HTTPRepository repository,String query) {
+		//Creamos la conexión
+		RepositoryConnection conexion = repository.getConnection();
+		System.out.println("Conexión realizada");
+		//Se prepara y evalúa query que pide id, nombre y URL de las ontolgías de OntologyDirectory.
+		TupleQuery tupleQuery = conexion.prepareTupleQuery(QueryLanguage.SPARQL,query);
+        TupleQueryResult tupleQueryResult = tupleQuery.evaluate();
+        System.out.println("Query evaluada");
+        // Each result is represented by a BindingSet, which corresponds to a result row
+        String res= null;
+        //Al ser iterable almacenamos los diferentes elementos en un string a devolver.
+        while(tupleQueryResult.hasNext()) {
+        	BindingSet bindingSet = tupleQueryResult.next();
+            // Each BindingSet contains one or more Bindings
+            for (Binding binding : bindingSet) {
+                // Each Binding contains the variable name and the value for this result row
+                String name = binding.getName();
+                Value value = binding.getValue();
+               res += name + " = " + value + "\n";       
+            }
+            res += "\n";
+        }
+        System.out.println("Respuesta impresa");
+        //Cerramos la tupla y las conexiones.
+        tupleQueryResult.close();
+        conexion.close();
+        System.out.println("Conexiones y querys cerradas");
+		return res;
+	}
+	
+	
+	
 	public static void main (String[] args) {
-		//Problema: Como Se incluyen las relaciones implícitas de las ontologías contenidas por la URL.
-		//TODO: ¿ Y si en vez de esto y que sean variables que terminen con la ejecución lo hacemos de modo que escriba en un fichero los valores y los lea también o quizás sería mejor emplear una base de datos relacional?
-		ArrayList<URL> list = new ArrayList<URL>();	
-		URL ex0 = null;
-		URL ex1 = null;
-		try {
-			ex0 = new URL(" https://www.google.es");
-			ex1 = new URL(" https://web.whatsapp.com");
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		list.add(ex0);
-		list.add(ex1);
+		
+		//Iniciamos la conexión con el servicio de SPARQL de GraphDB para hacer la query.
+		HTTPRepository repository = new HTTPRepository("http://localhost:7200/repositories/OntologyDirectory");
+		
 		
 		//GET para que devuelva el conjunto de URLs guardadas
-		get("/URL", (request, response) -> {
-			return list;
+		get("/OD", (request, response) -> {
+			String query = "BASE <http://localhost:4567/>\r\n"
+	                + "PREFIX foaf: <http://xmlns.com/foaf/0.1/>\r\n"
+	                + "SELECT ?x ?name ?url\r\n"
+	                + "WHERE {\r\n"
+	                + "    ?x foaf:name ?name.\r\n"
+	                + "    ?x foaf:homepage ?url\r\n"
+	                + "}\r\n"
+	                + "";
+			String res = llamadaDBGraph(repository,query);
+			return res;
 	     });
+
 		
 		//GET Para que busque un elemento dentro de la lista al incluirlo en la URL.
-		get("/URL/", (request, response) -> {
-			for (int i=0; i<list.size(); i++) {
-			    System.out.println(list.get(i).toString());
-				//System.out.println(request.splat()[0]);
-				String res = request.queryParams("URL");
-				System.out.println(res);
-				System.out.println(request.queryParams());
-			   // if(list.get(i).toString().equals(request.splat()[0]))
-				//	return list.get(i);
-			    if(list.get(i).toString().equals(res))
-					return list.get(i);
-			}	
-			return "El elemento introducido no se haya en la lista";
-		//TODO: Preguntar a Raúl y Andrea porque no sirve como parámetro de query pero sí como valor de atributo.
+		get("/OD/*", (request, response) -> {
+				StringBuffer sb = new StringBuffer();
+				for (int i=0; i<request.splat().length;i++)
+					sb.append(request.splat()[i]);
+				String name = sb.toString();
+				System.out.println(name);
+				String query = "BASE <http://localhost:4567/>\r\n"
+		                + "PREFIX foaf: <http://xmlns.com/foaf/0.1/>\r\n"
+		                + "SELECT ?x ?url\r\n"
+		                + "WHERE {\r\n"
+		                + "    ?x foaf:name \""+ name +"\".\r\n"
+		                + "    ?x foaf:homepage ?url\r\n"
+		                + "}\r\n"
+		                + "";
+				System.out.println(query);
+				String res = llamadaDBGraph(repository,query);
+				return res;
 	     });
 		
+		
+		//TODO: Adaptarlo a GraphDB
 		//POST Para crear una URL dentro de la lista de estas. 
-		post("/URL/add/", (request, response) -> {
-			URL elem = new URL(request.queryParams("URL"));
-			list.add(elem);
-			System.out.println(list);
-		    return "URL Uploaded";
+		post("/OD/", (request, response) -> {
+			String url = request.queryParams("url");
+			String name = request.queryParams("name"); 
+			String query = "BASE <http://localhost:4567/>\r\n"
+					+ "PREFIX foaf: <http://xmlns.com/foaf/0.1/>\r\n"
+					+ "INSERT DATA { \r\n"
+					+ "	<http://localhost:4567/#"+name.replace(" ","")+"> foaf:name \""+name+"\"; \r\n"
+					+ "    							foaf:homepage <"+url+">.\r\n"
+					+ "} ";
+			System.out.println(query);
+			String res = llamadaDBGraph(repository,query);
+		    return res;
 		});
 		
+		//TODO: Adaptarlo a GraphDB
 		///DELETE Para eliminar una URL de dentro de la lista de estas. 
-		delete("/URL/del/", (request, response) -> {
-		   for (int k = 0; k<list.size(); k++) {
+		delete("/OD/del/", (request, response) -> {
+		/*   for (int k = 0; k<list.size(); k++) {
 			   if (list.get(k).toString().equals(request.queryParams("URL"))) {
 				   list.remove(k);
 				   return "URL Removed";
 			   }
-		   }
+		   }*/
 		   return "URL not present in list";
 		});
 		
+		//TODO: Hacer PUT para Graph DB.
 		//PUT a estas alturas no tiene mucho sentido debido a que las URLs solo contienen un valor y actualizarlas sería equivalente a eliminar una y añadir otra asique no vamos a implementar la operación de momento.
 			
 		//--------------------------------------------------------------------------------------------------------------------------------------------------------------------
 		//AQUÍ COMENTA LA PARTE DEL SERVICIO EN LA QUE SE TRABAJA CON OWL COMO SE ESPECIFICA EN EL UC2
-		//TODO: Aprender a emplear GRAPHDB y conectarlo con el servicio
 		
-		
+		}
 	}
-}
