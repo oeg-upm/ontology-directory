b0d1efc95631c53ea8859336d88329938a895ae1
usuario <usuario@DESKTOP-OERVJCK.mshome.net>
2022-02-18 16:22:57 +0100

    Adaptado a Streamline

--- src/main/java/Service.java
+++ src/main/java/Service.java
@@ -1,26 +1,16 @@
 import static spark.Spark.*;
-import java.util.ArrayList;
 
-import org.eclipse.rdf4j.model.Value;
-import org.eclipse.rdf4j.query.Binding;
-import org.eclipse.rdf4j.query.BindingSet;
-import org.eclipse.rdf4j.query.QueryLanguage;
-import org.eclipse.rdf4j.query.TupleQuery;
-import org.eclipse.rdf4j.query.TupleQueryResult;
-import org.eclipse.rdf4j.repository.RepositoryConnection;
-import org.eclipse.rdf4j.repository.RepositoryException;
-import org.eclipse.rdf4j.repository.config.RepositoryConfigException;
-import org.eclipse.rdf4j.repository.http.HTTPRepository;
-import org.eclipse.rdf4j.rio.RDFHandlerException;
-import org.eclipse.rdf4j.rio.RDFParseException;
+import org.apache.jena.sparql.resultset.ResultsFormat;
 
-import com.ontotext.graphdb.example.util.EmbeddedGraphDB;
+import sparql.streamline.core.EndpointConfiguration;
+import sparql.streamline.core.SparqlEndpoint;
+import sparql.streamline.exception.SparqlQuerySyntaxException;
+import sparql.streamline.exception.SparqlRemoteEndpointException;
 
-import java.io.IOException;
-import java.net.*;
+import java.io.ByteArrayOutputStream;
 
 public class Service {
-	
+	/*
 	//Método auxiliar al que pasándole la query a realizar hace todo lo requerido 
 	public static String llamadaDBGraph(HTTPRepository repository,String query) {
 		//Creamos la conexión
@@ -52,36 +42,52 @@ public class Service {
 		return res;
 	}
 	
-	
+	*/
 	
 	public static void main (String[] args) {
-		
 		//Iniciamos la conexión con el servicio de SPARQL de GraphDB para hacer la query.
-		HTTPRepository repository = new HTTPRepository("http://localhost:7200/repositories/OntologyDirectory");
+		//HTTPRepository repository = new HTTPRepository("http://localhost:7200/repositories/OntologyDirectory");
+	
+		//Probamos con la librería de Andrea
+		//Creamos objeto Endpoint Configuration para acceder a DBGraph
+		EndpointConfiguration ec = new EndpointConfiguration();
+		String endpoint = "http://localhost:7200/repositories/OntologyDirectory";
+		String endpointupdate = "http://localhost:7200/repositories/OntologyDirectory/statements";
+		ec.setSparqlQuery(endpoint);
+		ec.setSparqlUpdate(endpointupdate);
+		SparqlEndpoint.setConfiguration(ec);	
+
 		
-		
-		//GET para que devuelva el conjunto de URLs guardadas
+		//GET para que devuelva el conjunto de tripletas guardadas
 		get("/OD", (request, response) -> {
 			String query = "BASE <http://localhost:4567/>\r\n"
-	                + "PREFIX foaf: <http://xmlns.com/foaf/0.1/>\r\n"
-	                + "SELECT ?x ?name ?url\r\n"
-	                + "WHERE {\r\n"
-	                + "    ?x foaf:name ?name.\r\n"
-	                + "    ?x foaf:homepage ?url\r\n"
-	                + "}\r\n"
-	                + "";
-			String res = llamadaDBGraph(repository,query);
+					+ "PREFIX foaf: <http://xmlns.com/foaf/0.1/>\r\n"
+					+ "SELECT ?x ?name ?url\r\n"
+					+ "WHERE {\r\n"
+					+ "    ?x foaf:name ?name.\r\n"
+					+ "    ?x foaf:homepage ?url\r\n"
+					+ "}";
+		
+			//Creamos objeto SPARQLEndpoint
+			String res = null;
+			try {
+				ByteArrayOutputStream res0 = SparqlEndpoint.query(query,ResultsFormat.FMT_RS_JSON);
+				res=res0.toString();
+			} catch (SparqlQuerySyntaxException e) {
+				e.printStackTrace();
+			} catch (SparqlRemoteEndpointException e) {
+				e.printStackTrace();
+			}
 			return res;
 	     });
 
 		
 		//GET Para que busque un elemento dentro de la lista al incluirlo en la URL.
-		get("/OD/*", (request, response) -> {
+		get("/OD/*/URL", (request, response) -> {
 				StringBuffer sb = new StringBuffer();
 				for (int i=0; i<request.splat().length;i++)
 					sb.append(request.splat()[i]);
 				String name = sb.toString();
-				System.out.println(name);
 				String query = "BASE <http://localhost:4567/>\r\n"
 		                + "PREFIX foaf: <http://xmlns.com/foaf/0.1/>\r\n"
 		                + "SELECT ?x ?url\r\n"
@@ -90,45 +96,98 @@ public class Service {
 		                + "    ?x foaf:homepage ?url\r\n"
 		                + "}\r\n"
 		                + "";
-				System.out.println(query);
-				String res = llamadaDBGraph(repository,query);
+				String res = null;
+				try {
+					ByteArrayOutputStream res0 = SparqlEndpoint.query(query,ResultsFormat.FMT_RS_JSON);
+					res=res0.toString();
+				} catch (SparqlQuerySyntaxException e) {
+					e.printStackTrace();
+				} catch (SparqlRemoteEndpointException e) {
+					e.printStackTrace();
+				}
 				return res;
 	     });
 		
 		
-		//TODO: Adaptarlo a GraphDB
 		//POST Para crear una URL dentro de la lista de estas. 
-		post("/OD/", (request, response) -> {
-			String url = request.queryParams("url");
-			String name = request.queryParams("name"); 
+		post("/OD/*/URL", (request, response) -> {
+			StringBuffer sb = new StringBuffer();
+			for (int i=0; i<request.splat().length;i++)
+				sb.append(request.splat()[i]);
+			String name = sb.toString();
+			String url = request.queryParams("url"); 
 			String query = "BASE <http://localhost:4567/>\r\n"
 					+ "PREFIX foaf: <http://xmlns.com/foaf/0.1/>\r\n"
 					+ "INSERT DATA { \r\n"
-					+ "	<http://localhost:4567/#"+name.replace(" ","")+"> foaf:name \""+name+"\"; \r\n"
+					+ "	<http://localhost:4567/"+name.replace(" ","")+"> foaf:name \""+name+"\"; \r\n"
 					+ "    							foaf:homepage <"+url+">.\r\n"
 					+ "} ";
-			System.out.println(query);
-			String res = llamadaDBGraph(repository,query);
-		    return res;
+			try {
+				SparqlEndpoint.update(query);
+			} catch (SparqlQuerySyntaxException e) {
+				e.printStackTrace();
+			} catch (SparqlRemoteEndpointException e) {
+				e.printStackTrace();
+			}
+			return "Tripleta Añadida";
 		});
 		
-		//TODO: Adaptarlo a GraphDB
+		
+		
 		///DELETE Para eliminar una URL de dentro de la lista de estas. 
-		delete("/OD/del/", (request, response) -> {
-		/*   for (int k = 0; k<list.size(); k++) {
-			   if (list.get(k).toString().equals(request.queryParams("URL"))) {
-				   list.remove(k);
-				   return "URL Removed";
-			   }
-		   }*/
-		   return "URL not present in list";
+		delete("/OD/*/URL", (request, response) -> {
+			StringBuffer sb = new StringBuffer();
+			for (int i=0; i<request.splat().length;i++)
+				sb.append(request.splat()[i]);
+			String name = sb.toString();
+			String query ="BASE <http://localhost:4567/>\r\n"
+					+ "PREFIX foaf: <http://xmlns.com/foaf/0.1/>\r\n"
+					+ "DELETE {\r\n"
+					+ "    ?x foaf:name ?name;\r\n"
+					+ "       foaf:homepage ?url.\r\n"
+					+ "}\r\n"
+					+ "WHERE{\r\n"
+					+ "     ?x foaf:name ?name;\r\n"
+					+ "       foaf:homepage ?url.\r\n"
+					+ "     FILTER(str(?name) = \""+name+"\") \r\n"
+					+ "}";
+			SparqlEndpoint.update(query);
+		   return "Tripleta Eliminada";
 		});
 		
 		//TODO: Hacer PUT para Graph DB.
-		//PUT a estas alturas no tiene mucho sentido debido a que las URLs solo contienen un valor y actualizarlas sería equivalente a eliminar una y añadir otra asique no vamos a implementar la operación de momento.
-			
+		//PUT para modificar los valores de la URL de una determinada ontología 
+		put("/OD/*/URL", (request, response) -> {
+			StringBuffer sb = new StringBuffer();
+			for (int i=0; i<request.splat().length;i++)
+				sb.append(request.splat()[i]);
+			String name = sb.toString();
+			String url = request.queryParams("url");
+			String query ="BASE <http://localhost:4567/>\r\n"
+					+ "PREFIX foaf: <http://xmlns.com/foaf/0.1/>\r\n"
+					+ "DELETE {\r\n"
+					+ "    ?x foaf:homepage ?url.\r\n"
+					+ "}\r\n"
+					+ "WHERE{\r\n"
+					+ "     ?x foaf:name ?name;\r\n"
+					+ "       foaf:homepage ?url.\r\n"
+					+ "     FILTER(str(?name) = \""+name+"\") \r\n"
+					+ "}";
+			System.out.println(query);
+			SparqlEndpoint.update(query);
+			String query1 = "BASE <http://localhost:4567/>\r\n"
+					+ "PREFIX foaf: <http://xmlns.com/foaf/0.1/>\r\n"
+					+ "INSERT DATA { \r\n"
+					+ "	<http://localhost:4567/"+name.replace(" ","")+"> foaf:homepage <"+url+">.\r\n"
+					+ "} ";
+			System.out.println(query1);
+			SparqlEndpoint.update(query1);
+		   return "Actualizada";
+		});
+		
+				
 		//--------------------------------------------------------------------------------------------------------------------------------------------------------------------
-		//AQUÍ COMENTA LA PARTE DEL SERVICIO EN LA QUE SE TRABAJA CON OWL COMO SE ESPECIFICA EN EL UC2
+		//AQUÍ COMIENZA LA PARTE DEL SERVICIO EN LA QUE SE TRABAJA CON OWL COMO SE ESPECIFICA EN EL UC2
 		
 		}
 	}
